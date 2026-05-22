91e0424cdca8ce444099a788d4314994d1ae05a2
Jade Guo <littleyu.hit@gmail.com>
2026-05-22 12:33:36 +0800

    delete ui

--- ui/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createPinia } from 'pinia';
-import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import './styles.css';
-import App from './App.vue';
-
-createApp(App).use(createPinia()).use(ElementPlus).mount('#app');
